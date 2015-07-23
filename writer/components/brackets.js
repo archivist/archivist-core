7e7e6ef03c49229f87b0b5c25064953397992a2a
Michael Aufreiter <michael@substance.io>
2015-07-23 13:24:41 +0200

    Reorganise ContentEditor.

---
+++ writer/components/brackets.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var $$ = React.createElement;
+
+class Brackets extends React.Component {
+  render() {
+    return $$("div", {}, '');
+  }
+}
+
+Brackets.displayName = "Brackets";
+module.exports = Brackets;
