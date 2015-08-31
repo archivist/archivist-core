75a419935e5cc34e872d0216bd4d0462816b5c33
Daniel Beilinson <beilinson@gmail.com>
2015-08-31 18:18:13 +0300

    use audio jplayer, refactor

--- reader/components/source_panel.js
+++ reader/components/source_panel.js
@@ -51,14 +51,52 @@ class SourcePanel extends Panel {
         $$('iframe', {src: src, frameBorder: "0", mozallowfullscreen: "true", allowFullScreen: "true"})
       );
     } else if (type == 'audio') {
-      return $$('ul',{className:"playlist"},
-        $$('li',{},
-          $$('a',{href: window.mediaServer + "/audio/"+id+".mp3"},"Test")
-        )
-      );
+      return this._renderAudioSource();
     } else {
       return $$('div',{}, "no source specified");
     }
+  }
+
+  _renderAudioSource() {
+    return $$('div', {className: 'audio-player'},
+      $$('div', {id: "jquery_jplayer_1", className: "jp-jplayer"}),
+      $$('div', {id: "jp_container_1", className: "jp-audio", role: "application", "aria-label": "media player"},
+        $$('div', {className: "jp-type-single"},
+          $$('div', {className: "jp-gui jp-interface"},
+            $$('div', {className: "jp-controls"},
+              $$('button', {className: "jp-play", role: "button", tabindex: "0"}, "play"),
+              $$('button', {className: "jp-stop", role: "button", tabindex: "0"}, "play")
+            ),
+            $$('div', {className: "jp-progress"},
+              $$('div', {className: "jp-seek-bar"},
+                $$('div', {className: "jp-play-bar"})
+              )
+            ),
+            $$('div', {className: "jp-volume-controls"},
+              $$('button', {className: "jp-mute", role: "button", tabindex: "0"}, "mute"),
+              $$('button', {className: "jp-volume-max", role: "button", tabindex: "0"}, "max volume"),
+              $$('div', {className: "jp-volume-bar"},
+                $$('div', {className: "jp-volume-bar-value"})
+              )
+            ),
+            $$('div', {className: "jp-time-holder"},
+              $$('div', {className: "jp-current-time", role: "timer", "aria-label": "time"}, " "),
+              $$('div', {className: "jp-duration", role: "timer", "aria-label": "duration"}, " "),
+              $$('div', {className: "jp-toggles"},
+                $$('button', {className: "jp-repeat", role: "button", tabindex: "0"}, "repeat")
+              )
+            )
+          ),
+          $$('div', {className: "jp-details"},
+            $$('div', {className: "jp-title", "aria-label": "title"}, " ")
+          ),
+          $$('div', {className: "jp-no-solution"},
+            $$('span', {}, "Update Required"),
+            $$('p', {}, 'To play the media you will need to either update your browser to a recent version or update your Flash plugin')
+          )
+        )
+      )
+    );
   }
 
   _renderLabelValue(metadata, prop, label) {
@@ -75,20 +113,63 @@ class SourcePanel extends Panel {
   updateSourceTime(init) {
     var self = this;
     var app = this.context.app;
+    var doc = this.props.doc;
+    var metadata = doc.getDocumentMeta();
+    var id = metadata.media_id;
+    var player = metadata.record_type;
     var time = app.state.time;
-    if(time) {
-      var iframe = $('iframe')[0];
-      var player = $f(iframe);
-      if(init){
-        player.addEvent('ready', function() {
-          player.api('seekTo', self.hmsToSecondsOnly(time));
-          player.api('pause');
-        });
-      } else {
+
+    if(init) {
+      if(player == 'video') {
+        this.initVideoPlayer(time);
+      } else if (player == 'audio') {
+        this.initAudioPlayer(time, id);
+      }
+    } else if (time) {
+      if(player == 'video') {
+        var iframe = $('iframe')[0];
+        var player = $f(iframe);
         player.api('seekTo', this.hmsToSecondsOnly(time));
         player.api('play');
+      } else if (player == 'audio') {
+        var player = $("#jquery_jplayer_1");
+        player.jPlayer("play", this.hmsToSecondsOnly(time));
       }
     }
+
+  }
+
+  initVideoPlayer(time) {
+    var iframe = $('iframe')[0];
+    var player = $f(iframe);
+    var seekTo = time || 0;
+    if(seekTo != 0) seekTo = this.hmsToSecondsOnly(seekTo); 
+    player.addEvent('ready', function() {
+      player.api('seekTo', seekTo);
+      player.api('pause');
+    });
+  }
+
+  initAudioPlayer(time, id) {
+    var seekTo = time || 0; 
+    if(seekTo != 0) seekTo = this.hmsToSecondsOnly(seekTo); 
+    $("#jquery_jplayer_1").jPlayer({
+      ready: function (event) {
+        $(this).jPlayer("setMedia", {
+          mp3: window.mediaServer + "/audio/" + id + ".mp3",
+          oga: window.mediaServer + "/audio/" + id + ".ogg"
+        });
+        $(this).jPlayer("pause", seekTo);
+      },
+      supplied: "oga, mp3",
+      wmode: "window",
+      useStateClassSkin: true,
+      autoBlur: false,
+      smoothPlayBar: true,
+      keyEnabled: true,
+      remainingDuration: true,
+      toggleDuration: true
+    });
   }
 
   hmsToSecondsOnly(str) {
